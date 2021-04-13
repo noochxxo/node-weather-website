0bc5ab02c548c0ffd121753d4de2e6a90b5cc1c4
Eric Trotchie <Eric-JT@users.noreply.github.com>
2021-04-13 01:20:06 -0700

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,92 @@
+const path = require('path');
+require('dotenv').config();
+const express = require('express');
+const hbs = require('hbs');
+const geocode = require('../src/utils/geocode');
+const forecast = require('../src/utils/forecast');
+
+const app = express();
+
+// define Paths for express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialPath = path.join(__dirname, '../templates/partials');
+
+// setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialPath);
+
+// sets up the static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Local Weather Forecast',
+        name: 'Eric'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Eric'
+    });
+});
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        message: 'This is the help page. You need help?',
+        name: 'Eric'
+    });
+});
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address'
+        });
+    }
+    geocode(req.query.address, (error, {latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({
+                error
+            });
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.render('404', {
+                    error,
+                    title: 'oopsie',
+                    name: 'Eric'
+                });
+            }
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            });
+        });
+    });
+});
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        error: 'This is not the help page you are looking 404',
+        title: 'Oopsie!',
+        name: 'Eric'
+    });
+});
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        error: 'This is not the page you are looking 404',
+        title: 'Oopsie!',
+        name: 'Eric'
+    });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port: ${process.env.PORT}`)
+});
